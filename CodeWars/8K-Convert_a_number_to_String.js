8871ecef01a1cd01cad4e6a6fa5974c9b55a4dd7
shanf-gh <shan.fabien@gmail.com>
2022-03-17 23:52:30 +1300

    Completed kata

--- CodeWars/8K-Convert_a_number_to_String.js
+++ CodeWars/8K-Convert_a_number_to_String.js
@@ -2,3 +2,17 @@
 Level: 8 kyuu
 Link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
 */
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+  }
+
+/* Top solutions analysis */
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return String(num);
+}
+
+// This solution is using the String object to convert the value into string
