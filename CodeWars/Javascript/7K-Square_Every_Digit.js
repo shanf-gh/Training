6da14926240886586071bb484ef0f0b817958ded
shanf-gh <shan.fabien@gmail.com>
2022-03-24 23:51:52 +1300

    Kata completed

---
+++ CodeWars/Javascript/7K-Square_Every_Digit.js
@@ -0,0 +1,13 @@
+/* 
+Level: 7 kyu
+Link: https://www.codewars.com/kata/546e2562b03326a88e000020
+*/
+
+function squareDigits(num){
+    return Number(num.toString().split('').map(x=>x*x).join(''))
+}
+
+// My answer is very similar to the top solution
+
+/* Top solutions analysis */
+
