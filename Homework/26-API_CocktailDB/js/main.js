255a25a70512157d0fe70182be9d021ca7e348a0
shanf-gh <shan.fabien@gmail.com>
2022-04-10 23:28:21 +1200

    Added api response loop to create a drink card for each drink in the array. Added card number to card

--- Homework/26-API_CocktailDB/js/main.js
+++ Homework/26-API_CocktailDB/js/main.js
@@ -1,9 +1,49 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-function CreateDrinkCard(name, thumbnail, ingredients, instructions) {
+function CreateDrinkCard(name, drinkThumb, ingredients, instructions) {
     this.name = name;
     this.drinkThumb = drinkThumb;
     this.ingredients = ingredients;
     this.instructions = instructions;
+}
+
+function generateDrinkCard(obj, index, numberOfCards) {
+    return `
+        <section class="drink-card">
+            <div class="drink-card-number">${index}/${numberOfCards}</div>
+            <h2>${obj.name}</h2>
+            <section class="drink-info">
+                <div class="left-panel">
+                    <img src="${obj.drinkThumb}" alt="">
+                </div>
+                <div class="right-panel">
+                    <section class="list-ingredients">
+                        <h3>Ingredients</h3>
+                        ${obj.ingredients.reduce((acc, val) => acc += `<li>${val}</li>`, '')}
+                    </section>
+                    <section class="cocktail-instructions">
+                        <h3>Instructions</h3>
+                        <p>${obj.instructions}</p>
+                    </section>
+                </div>
+            </section>
+        </section>
+    `;
+}
+
+function generateIngredientsArray(obj) {
+    const drink = obj;
+    let ingredients = [];
+    for(let i = 1; i < 15; i++) {
+        let measure = drink[`strMeasure${i}`];
+        let ingredient = drink[`strIngredient${i}`];
+        
+        if(ingredient === null) {
+            break;
+        }
+        ingredients.push(measure || '' + ingredient);
+    }
+
+    return ingredients;
 }
 
 function searchDrink() {
@@ -13,35 +53,16 @@ function searchDrink() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink)
     .then(res => res.json())
     .then(data => {
-        let drinkCard = document.querySelector('.drink-card')
-
-        let drink = data.drinks[0];
-
-        // create an array of ingredients
-        let ingredients = [];
-        for(let i = 1; i < 15; i++) {
-            let measure = drink[`strMeasure${i}`];
-            let ingredient = drink[`strIngredient${i}`];
-            
-            if(measure === null) {
-                measure = '';
-            }
-
-            if(ingredient!==null) {
-                ingredients.push(measure + ingredient);
-            }
-        }
-
-
-        // set values in HTML elements
-        drinkCard.querySelector('h2').innerText = drink.strDrink;
-        drinkCard.querySelector('img').src = drink.strDrinkThumb;
-        drinkCard.querySelector('.list-ingredients > ul').innerHTML = ingredients.reduce((acc, val) => acc += `<li>${val}</li>`, '');
-        drinkCard.querySelector('.cocktail-instructions p').innerText = drink.strInstructions;
-        // Show the card
-        // drinkCard.style.display = "block";
-
-        console.log(data);
+        const drinkContainer = document.querySelector('.drink-card-container');
+        const drinks = data.drinks;
+        
+        drinks.forEach((drink, index) => {
+            let ingredients = generateIngredientsArray(drink);
+        
+            let newDrink = new CreateDrinkCard(drink.strDrink, drink.strDrinkThumb, ingredients, drink.strInstructions)
+            // append new drink card
+            drinkContainer.innerHTML += generateDrinkCard(newDrink, index + 1, drinks.length);
+        });
     })
     .catch(err => {
         console.log(`error ${err}`);
