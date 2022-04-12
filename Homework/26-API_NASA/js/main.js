a3a1b5d4bc9aa25fd9286d68941d8aefb21ac8f8
shanf-gh <shan.fabien@gmail.com>
2022-04-12 21:59:50 +1200

    Testing config.js being ignored

---
+++ Homework/26-API_NASA/js/main.js
@@ -0,0 +1,19 @@
+// Get API Key
+const key = config.API_TOKEN;
+
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
