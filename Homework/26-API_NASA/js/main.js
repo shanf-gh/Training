bcb83c77733bd240efe166358be1c16cef17e2ae
shanf-gh <shan.fabien@gmail.com>
2022-04-12 22:03:49 +1200

    Changed fetch url

--- Homework/26-API_NASA/js/main.js
+++ Homework/26-API_NASA/js/main.js
@@ -6,7 +6,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
