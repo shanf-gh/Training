70d072c1d8fca4a5e513292d3bc1f12d704f9ebf
shanf-gh <shan.fabien@gmail.com>
2022-04-12 23:01:53 +1200

    Initial data fetch and DOM manipulation

--- Homework/26-API_NASA/js/main.js
+++ Homework/26-API_NASA/js/main.js
@@ -1,17 +1,25 @@
 // Get API Key
-const key = config.API_TOKEN;
+const key = config.API_KEY;
 
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+  const date = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`
 
+  console.log(date);
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        console.log(data);
+        if('copyright' in data) {
+          document.querySelector('p').innerText = `Photo by ${data.copyright}`;
+        }
+
+        document.querySelector('h2').innerText = data.title;
+        document.querySelector('img').src = data.url;
+        document.querySelector('h3').innerText = data.explanation;
       })
       .catch(err => {
           console.log(`error ${err}`)
