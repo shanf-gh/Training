e15759878871207c12049b64010000c096cb4066
shanf-gh <shan.fabien@gmail.com>
2022-04-21 23:58:25 +1200

    Added special price function as not every type of item has a special price

--- Homework/26-AnimalCrossing/js/main.js
+++ Homework/26-AnimalCrossing/js/main.js
@@ -1,39 +1,56 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
-let test = '';
-function getFetch(){
-  // const choice = document.querySelector('input').value
-  const url = 'https://acnhapi.com/v1/fish/'
+// document.querySelector('button').addEventListener('click', getFetch);
+
+document.querySelector('form').addEventListener('submit', getData, false);
+
+function getData(event) {
+  const data = Object.fromEntries(new FormData(event.target).entries());
+  getFetch(data);
+  console.log(data);
+  event.preventDefault();
+}
+
+
+function getFetch(formData){
+  const url = `https://acnhapi.com/v1/${formData['type']}`
+  
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data);
-        // console.log(data['angelfish']);
+        const cardTable = document.querySelector(".card-table");
+        clearElement(cardTable)
         for(let key in data) {
-          document.querySelector(".card-table").innerHTML += createCard(data[key]);
+          cardTable.innerHTML += createCard(data[key], formData) || '';
         }
-        // document.querySelector(".fish-list").innerHTML += `<li>${data[0].name['name']}${}</li>`;
-        
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
-function createCard(obj) {
-  // console.log(obj);
+function clearElement(element) {
+  element.innerHTML = '';
+}
+
+function createCard(obj, formData) {
+  
+  const monthAvailability = obj['availability'][`month-array-${formData['hemisphere']}`];
+  const month = formData['month-select'] || '';
+  if(month !=='' && !monthAvailability.includes(+month)) {
+    return false;
+  }
+
   return `<section class="fish-card">
 				<section class="name">
 					<h3>${obj['name']['name-USen']}</h3>
-					<img src="https://acnhapi.com/v1/icons/fish/${obj['id']}" alt="obj['name']['name-USen']">
+					<img src="https://acnhapi.com/v1/icons/${formData['type']}/${obj['id']}" alt="obj['name']['name-USen']">
 				</section>
 				<section class="availability">
           <section class="seasonality">
             <h3>Seasonality</h3>
             <section>
               <table class="availability-month">
-                ${getSeasonalityRows(obj['availability']['month-array-northern'])}
+                ${getSeasonalityRows(monthAvailability)}
               </table>
             </section>
           </section>
@@ -54,49 +71,16 @@ function createCard(obj) {
                 <h4>Regular</h4>
                 <span>${obj['price']}</span>
               </div>
-              <div>
-                <h4>CJ</h4>
-                <span>${obj['price-cj']}</span>
-              </div>
+              ${getSpecialPrice(obj) || ''}
               </section>
             </section>
 				</section>
 			</section>`;
 }
 
-// function getSeasonalityRows(monthsArray) {
-//   const months = ['Jan','Feb','Mar.','Apr.','May','June','July','Aug.','Sep.','Oct.','Nov.','Dec.'];
-
-//   monthsArray = monthsArray.sort((a, b) => a-b);
-
-//   return `
-//     <tr>
-//       <td><div>Jan</div></td>
-//       <td><div>Feb.</div></td>
-//       <td><div>Mar.</div></td>
-//       <td><div>Apr.</div></td>
-//     </tr>
-//     <tr>
-//       <td><div>May</div></td>
-//       <td><div>June</div></td>
-//       <td><div>July</div></td>
-//       <td><div>Aug.</div></td>
-//     </tr>
-//     <tr>
-//       <td><div>Sep.</div></td>
-//       <td><div>Oct.</div></td>
-//       <td><div>Nov.</div></td>
-//       <td><div>Dec.</div></td>
-//     </tr>
-//   `
-// }
-
 function getSeasonalityRows(monthsArray) {
   const months = ['Jan','Feb','Mar.','Apr.','May','June','July','Aug.','Sep.','Oct.','Nov.','Dec.'];
-  
-	// // sort parameter array
-  // monthsArray = monthsArray.sort((a, b) => a-b);
-  
+    
   let dataRow = months.map((month, index) => `<td><div ${monthsArray.includes(index+1) ? `class='active'` : ''}>${month}</div></td>`);
 
   return dataRow.reduce((acc, el, index) =>{
@@ -111,4 +95,27 @@ function getSeasonalityRows(monthsArray) {
     }
     return acc;
   },'');
+}
+
+function getSpecialPrice(obj) {
+  let vendor = '' ;
+  let price = '';
+
+  if(obj['price-cj']) {
+    vendor = 'CJ';
+    price = obj['price-cj'];
+  } else if (obj['price-flick']) {
+    vendor = 'Flick';
+    price = obj['price-flick'];
+
+  } else {
+    return false;
+  }
+
+  return `
+  <div>
+    <h4>${vendor}</h4>
+    <span>${price}</span>
+  </div>
+  `;
 }
