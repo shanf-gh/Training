281a78be2b9b134de6786245a39a40b36a4484d0
shanf-gh <shan.fabien@gmail.com>
2022-05-15 17:06:20 +1200

    Kata completed

---
+++ CodeWars/Javascript/8K-Reversed_sequence.js
@@ -0,0 +1,15 @@
+/* 
+Level: 8 kyuu
+Link: https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript
+*/
+
+const reverseSeq = n => {
+    let arr = [];
+    
+    for(let i = n; i > 0; i--) {
+      arr.push(i);
+    }
+    return arr;
+};
+
+/* Top solutions analysis */
