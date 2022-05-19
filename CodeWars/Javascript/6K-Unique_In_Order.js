cc181311f70e2e07d2c6ffefdc24baf31213eb16
shanf-gh <shan.fabien@gmail.com>
2022-05-20 00:02:04 +1200

    Updated

--- CodeWars/Javascript/6K-Unique_In_Order.js
+++ CodeWars/Javascript/6K-Unique_In_Order.js
@@ -1,7 +1,28 @@
 /* 
-Level: 8 kyuu
-Link: 
+Level: 6 kyuu
+Link: https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
 */
 
+var uniqueInOrder=function(iterable){
+    //your code here - remember iterable can be a string or an array
+    // check if array, if not split into array
+    if(!Array.isArray(iterable)) {
+      iterable = iterable.split('');
+    }
+    
+    let uniqueInOrder = iterable.reduce((arr, val) => {
+      if(arr.length === 0 ) {
+        arr.push(val);
+      } else {
+        if(arr[arr.length -1 ] !== val) {
+          arr.push(val)
+        }  
+      }
+      return arr;
+    }, []);
+    
+    // return result array
+    return uniqueInOrder;
+}
 
 /* Top solutions analysis */
