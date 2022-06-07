87f98d60f809e4e4446db432e5f9096f9f16da74
shanf-gh <shan.fabien@gmail.com>
2022-06-07 23:06:30 +1200

    Kata completed

---
+++ CodeWars/Javascript/8K/8K-Removing_elements.js
@@ -0,0 +1,10 @@
+/* 
+Level: 8 kyuu
+Link: https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/javascript
+*/
+
+function removeEveryOther(arr){
+    return arr.filter((x, i) => !(i % 2));
+}
+
+/* Top solutions analysis */
