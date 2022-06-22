266506e85a19373134faadc89a14de60b5f98bb1
shanf-gh <shan.fabien@gmail.com>
2022-06-22 23:25:25 +1200

    Kata completed

---
+++ CodeWars/Javascript/6K/6K-Two_Sum.js
@@ -0,0 +1,20 @@
+/* 
+Level: 6 kyu
+Link: https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+*/
+
+function twoSum(numbers, target) {
+    for(let i = 0; i < numbers.length; i++) {
+      for(let j = i + 1; j < numbers.length; j++) {
+        if(numbers[i] + numbers[j] === target) {
+          return [i,j];
+        }
+      }  
+    }
+}
+
+/* Top solutions analysis */
+
+// ################################
+// TOP SOLUTION 1 
+// ################################
