226139630f10a57af38d992d1e269adf31a32e64
shanf-gh <shan.fabien@gmail.com>
2022-06-27 23:55:03 +1200

    Kata completed

---
+++ CodeWars/Javascript/8K/8K-Grasshopper-Check_for_factor.js
@@ -0,0 +1,10 @@
+/* 
+Level: 8 kyuu
+Link: https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+*/
+
+function checkForFactor (base, factor) {
+    return base % factor === 0;
+  }
+
+/* Top solutions analysis */
