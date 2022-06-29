d22bd6316949d5f01e711e0d69555ee40add12a4
shanf-gh <shan.fabien@gmail.com>
2022-06-30 00:45:01 +1200

    Kata completed

---
+++ CodeWars/Javascript/7K/7K-Sum_of_all_the_multiples_of_3_or_5.js
@@ -0,0 +1,16 @@
+/* 
+Level: 7 kyuu
+Link: https://www.codewars.com/kata/57f36495c0bb25ecf50000e7/javascript
+*/
+function findSum(n) {
+    let sum = 0;
+    
+    for(let i = 0; i <= n; i++){
+      if(i % 3 === 0 || i % 5 === 0){
+        sum += i;
+      }
+    }
+    
+    return sum;
+  }
+/* Top solutions analysis */
