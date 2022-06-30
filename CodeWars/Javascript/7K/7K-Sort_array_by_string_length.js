ba3c4a0b592d6009195b9fe10bf5575c8e6c36bb
shanf-gh <shan.fabien@gmail.com>
2022-07-01 01:56:02 +1200

    Kata completed

---
+++ CodeWars/Javascript/7K/7K-Sort_array_by_string_length.js
@@ -0,0 +1,10 @@
+/* 
+Level: 7 kyuu
+Link: https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
+*/
+
+function sortByLength (array) {
+    return array.sort((a,b) => a.length - b.length);
+};
+
+/* Top solutions analysis */
