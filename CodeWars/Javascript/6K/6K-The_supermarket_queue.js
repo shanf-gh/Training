333661a13e7dc660f0312de1055dc1745c924edf
shanf-gh <shan.fabien@gmail.com>
2022-07-05 23:54:16 +1200

    Kata completed

--- CodeWars/Javascript/6K/6K-The_supermarket_queue.js
+++ CodeWars/Javascript/6K/6K-The_supermarket_queue.js
@@ -35,3 +35,11 @@ function queueTime(customers, n) {
 // ################################
 // TOP SOLUTION 1 
 // ################################
+function queueTime(customers, n) {
+    var w = new Array(n).fill(0);
+    for (let t of customers) {
+      let idx = w.indexOf(Math.min(...w));
+      w[idx] += t;
+    }
+    return Math.max(...w);
+}
