3a553f47dc4294086de121e22809d144641dbfe5
shanf-gh <shan.fabien@gmail.com>
2022-07-08 01:51:55 +1200

    Kata completed

---
+++ CodeWars/Javascript/7K/7K-No_oddities_here.js
@@ -0,0 +1,11 @@
+/* 
+Level: 7 kyuu
+Link: https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/javascript
+*/
+
+function noOdds( values ){
+    // Return all non-odd values
+    return values.filter(x => x % 2 === 0);
+  }
+
+/* Top solutions analysis */
